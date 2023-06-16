467c9b9699207383e808adc7aa411c5e1d0222ef
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-16 22:02:57 +0545

    setup mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import {type AppType} from "next/app";
 import {api} from "~/utils/api";
 import "~/styles/globals.css";
 import Header from "~/components/Header";
+import {MantineProvider} from "@mantine/core";
 
 const MyApp: AppType = ({Component, pageProps}) => {
-    return <>
+    return <MantineProvider withGlobalStyles withNormalizeCSS theme={{loader: 'oval'}}>
         <Header/>
         <Component {...pageProps} />
-    </>;
+    </MantineProvider>;
 };
 
 export default api.withTRPC(MyApp);
