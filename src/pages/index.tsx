f4c65c706e7adc69c41e8aeafdd675235239e968
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-16 22:46:06 +0545

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,5 +15,5 @@ export default function Home() {
                 className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
             </main>
         </>
-    );
+    )
 }
