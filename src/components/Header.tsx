19ae2f3bc48a87716b3818b26c0f11aabe429e41
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-16 19:44:22 +0545

    setup planetscale db

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type {NextComponentType} from "next";
+
+const Header : NextComponentType = () => {
+    return (
+        <header className='w-full h-64 bg-red-900'>
+
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
+import {type AppType} from "next/app";
+import {api} from "~/utils/api";
 import "~/styles/globals.css";
+import Header from "~/components/Header";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({Component, pageProps}) => {
+    return <>
+        <Header/>
+        <Component {...pageProps} />
+    </>;
 };
 
 export default api.withTRPC(MyApp);
